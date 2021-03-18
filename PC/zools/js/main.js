746d9287704dcc28eee9f5733a6d7fa4b355dbb5
deurensis <66526039+deurensis@users.noreply.github.com>
2021-03-18 11:46:31 +0900

    Add files via upload

---
+++ PC/zools/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    // 메인 슬라이드
+
+    $('.main_visual').slick({
+        infinite: true,
+        autoplay: 5000,
+        speed: 1000,
+        dots: true,
+        pauseOnHover : false,
+        draggable : false,
+      });
+
+});
